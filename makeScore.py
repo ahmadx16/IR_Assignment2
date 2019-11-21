9d4a6be6a9a35006c1851a2ede07da22b1736664
Ahmed Hasan <ahmadx16@hotmail.com>
2019-11-21 08:58:35 +0500

    corrected the syntax according to given

--- makeScore.py
+++ makeScore.py
@@ -6,6 +6,13 @@ import os
 import math
 # read stopwords 
 
+scoreName='Dirichlet'
+
+if len(sys.argv)>=3:
+    if sys.argv[1]=='--score':
+        scoreName=sys.argv[2]
+
+
 fStoplist = open("stoplist.txt","r")
 
 stopWordList = str(fStoplist.read()).split()
@@ -93,7 +100,7 @@ AvgDocLen = TotalCorpusWords/(len(allDocs)-1)
 
 
 
-#######  Score Function 1 (BM25)
+
 
 tidFile = open('termid.txt','r')
 tids = tidFile.readlines()
@@ -114,74 +121,98 @@ didFile.close()
 #print(dicdids[str(1)])
 
 qkeys = querydict.keys()
-for qkey in qkeys:
-    q1 = querydict[qkey]
+#######  Score Function 1 (BM25)
+if scoreName=="BM25":
+    scorefilebm = open('scorefilebm.txt','w')
+    qkeys = querydict.keys()
+    for qkey in qkeys:
+        q1 = querydict[qkey]
 
-    CorpScore = [0 for i in range(len(allDocs))]
+        CorpScore = [0 for i in range(len(allDocs))]
 
-    k1=  1.2
-    k2 = 50
-    b = 0.75
+        k1=  1.2
+        k2 = 50
+        b = 0.75
 
-    relDocs =[]
-    for q in q1:
-        qid = termId[q]
-        qpos = termDic[qid]                 # getting postings
-        relDocs.append(qpos[2].keys())
+        relDocs =[]
+        for q in q1:
+            qid = termId[q]
+            qpos = termDic[qid]                 # getting postings
+            relDocs.append(qpos[2].keys())
 
-    #len(termDic[termId[q1[i]]][2][docid]) is term freq
-    i=0  
-    for doci in relDocs:
-        for docid in doci:
-            K = k1*((1-b) + (b*(allDocs[docid]/AvgDocLen)))     # d=> doc len
-            tscore  = math.log10((allDocs[docid] + 0.5)/IDF[int(qid)-1] +0.5) * ((1+k1)*len(termDic[termId[q1[i]]][2][docid])/K + len(termDic[termId[q1[i]]][2][docid])) * ((1+k2)/(k2+1))
-            CorpScore[docid]+=tscore
-        i+=1
+        #len(termDic[termId[q1[i]]][2][docid]) is term freq
+        i=0  
+        for doci in relDocs:
+            for docid in doci:
+                K = k1*((1-b) + (b*(allDocs[docid]/AvgDocLen)))     # d=> doc len
+                tscore  = math.log10((allDocs[docid] + 0.5)/IDF[int(qid)-1] +0.5) * ((1+k1)*len(termDic[termId[q1[i]]][2][docid])/K + len(termDic[termId[q1[i]]][2][docid])) * ((1+k2)/(k2+1))
+                CorpScore[docid]+=tscore
+            i+=1
 
-    temparr = []
-    for i in range(1,len(allDocs)):
-        temparr.append([i,CorpScore[i]])
-    
-    temparr.sort(reverse=True,key=lambda x: x[1])
+        temparr = []
+        for i in range(1,len(allDocs)):
+            temparr.append([i,CorpScore[i]])
+        
+        temparr.sort(reverse=True,key=lambda x: x[1])
 
-    scorefilebm = open('scorefilebm.txt','a')
-    for i in range(0,len(allDocs)-1):
-        scorefilebm.write(str(qkey) +' '+dicdids[str(temparr[i][0])] +' '+str(temparr[i][1])+'\n' )
+        
+        for i in range(0,len(allDocs)-1):
+            scorefilebm.write(str(qkey) +' '+dicdids[str(temparr[i][0])] +' '+ str(i+1) +' '+str(temparr[i][1])+' '+ 'run3'+'\n' )
 
     scorefilebm.close()
 
 #######  Score Function 2 (Dirichlet)
 
 ## N/N+u  (doc)  + u/(N+u) (corpus)
+if scoreName=="Dirichlet":
+    scorefiledir = open("scorefiledir.txt",'w') 
+    for qkey in qkeys:
+        q1 = querydict[qkey]
 
-print (querydict)
- 
-for qkey in qkeys:
-    q1 = querydict[qkey]
+        CorpScore = [0 for i in range(len(allDocs))]
 
-    CorpScore = [0 for i in range(len(allDocs))]
-
-    relDocs =[]
-    lenrelDocs=[]
-    BackGroundtp=[]
-    for q in q1:
-        qid = termId[q]
-        BackGroundtp.append(BackGroundProbability[int(qid)])
-        qpos = termDic[qid]                  # getting postings
-        relDocs.append(qpos[2].keys())
-        tmparr=[]
-        for k in qpos[2].keys():
-            tmparr.append(len(qpos[2][k]))
-        lenrelDocs.append(tmparr)
+        relDocs =[]
+        lenrelDocs=[]
+        BackGroundtp=[]
+        for q in q1:
+            qid = termId[q]
+            BackGroundtp.append(BackGroundProbability[int(qid)])
+            qpos = termDic[qid]                  # getting postings
+            relDocs.append(qpos[2].keys())
+            tmparr=[]
+            for k in qpos[2].keys():
+                tmparr.append(len(qpos[2][k]))
+            lenrelDocs.append(tmparr)
 
 
-    i=0  
-    for doci in relDocs:
-        for docid in doci:
-            j=0
-            tscore  = (lenrelDocs[i][j]/allDocs[docid])*allDocs[docid]/(allDocs[docid]+AvgDocLen) + (BackGroundtp[i]) *AvgDocLen/ (allDocs[docid]+AvgDocLen)
-            CorpScore[docid]+=tscore
-            j+=1
-        i+=1
+        i=0  
+        for doci in relDocs:
+            for docid in doci:
+                j=0
+                tscore  = (lenrelDocs[i][j]/allDocs[docid])*allDocs[docid]/(allDocs[docid]+AvgDocLen) + (BackGroundtp[i]) *AvgDocLen/ (allDocs[docid]+AvgDocLen)
+                CorpScore[docid]+=tscore
+                j+=1
+            i+=1
 
-    print(CorpScore)
+        
+        temparr = []
+        for i in range(1,len(allDocs)):
+            temparr.append([i,CorpScore[i]])
+        
+        temparr.sort(reverse=True,key=lambda x: x[1])
+
+        ##  Evaluation
+
+
+    
+        for i in range(0,len(allDocs)-1):
+            scorefiledir.write(str(qkey) +' '+dicdids[str(temparr[i][0])] +' '+ str(i+1)+' ' +str(temparr[i][1])+' '+ 'run3'+'\n' )
+
+
+    scorefiledir.close()
+
+
+
+
+
+    
